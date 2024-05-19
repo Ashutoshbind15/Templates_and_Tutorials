d7dc0d924f6f33f0e857cae5a249b96f117a3194
Ashutoshbind15 <Ashutoshbind15@users.noreply.github.com>
2024-05-19 13:16:28 +0530

    readd titles

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default async function Home() {
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+        <h1>Temps and tutorials</h1>
         <div className="p-2 border-2 border-white">
           {populatedRepos.map((repo: any) => {
             return (
