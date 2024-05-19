696221c3b5ea64e73d6e511587244d35307d9c67
Ashutoshbind15 <91549538+Ashutoshbind15@users.noreply.github.com>
2024-05-19 11:47:03 +0530

    re-ping git on local

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -41,7 +41,7 @@ export default async function Home() {
   return (
     <>
       <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <div>Repos</div>
+        <div>Repo</div>
 
         <div className="p-2 border-2 border-white">
           {populatedRepos.map((repo: any) => {
